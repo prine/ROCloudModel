8af68681bd6c577db5533018de2fef444c4059d1
Robin Oster <robin.oster@rascor.com>
2016-02-02 13:25:59 +0100

    Initial Commit

---
+++ ROCloudModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROCloudModel
+//
+//  Created by Robin Oster on 02/02/16.
+//  Copyright © 2016 Prine Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
